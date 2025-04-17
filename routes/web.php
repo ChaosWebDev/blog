c531bde35c8d0835fd7f8b33e4df898d2e58a9a7
jpgerber1986 <jordangerber@gmail.com>
2025-04-17 11:09:27 -0600

    Updated Routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
+use App\Livewire\Post\Edit;
 use App\Livewire\Auth\Login;
+use App\Livewire\Post\Index;
+use App\Livewire\Post\Create;
 use Illuminate\Support\Facades\Route;
 
 // ! AUTHENTICATED ROUTES ! //
 Route::middleware('auth')->prefix('authd')->as('authd.')->group(function () {
     Route::get('/', fn() => "Authenticated Home")->name('home');
+
+    Route::prefix('/post')->as('posts.')->group(function () {
+        Route::get('/', Index::class)->name('index');
+        Route::get('/create', Create::class)->name('create');
+        Route::get('/{stub}/edit', Edit::class)->name('edit');
+    });
 });
 
 
