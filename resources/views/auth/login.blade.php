9cf613d05aca2391e450b9a02b7a3a970ef3e4bb
jpgerber1986 <jordangerber@gmail.com>
2025-04-17 10:35:52 -0600

    Fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,10 @@
             <x-input name='username' label="Username" />
             <x-input name='password' type='password' label="Password" />
 
+            <div class="flex-row justify-center gap-md col-2">
+                <x-input name='remember' type='checkbox' label="Remember Me" />
+            </div>
+
             <div class="buttons">
                 <input type="submit" value="Login" class='btn primary'>
             </div>
