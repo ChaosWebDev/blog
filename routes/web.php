8c015d449ef96d0bd94a166aa564acf8641828bf
jpgerber1986 <jordangerber@gmail.com>
2025-04-17 10:08:57 -0600

    Formatted Routing Placeholders

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Livewire\Auth\Login;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// ! LOGIN ROUTES ! //
+Route::middleware('guest')->group(function () {
+    Route::get('/login', Login::class)->name('login');
 });
+
+// ! MUST BE AUTHENTICATED ROUTES ! //
+Route::middleware('auth')->group(function () {
+    //
+});
+
+// ! OPEN TO PUBLIC ROUTES ! //
+// TODO - [[PLACEHOLDER]] - TODO //
