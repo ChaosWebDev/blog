a03e92ca4b3f497b5a4bd2158fc130c1f8947e6f
jpgerber1986 <jordangerber@gmail.com>
2025-04-17 14:39:50 -0600

    Updated for Authenticated for editing

--- app/Livewire/Components/Menu.php
+++ app/Livewire/Components/Menu.php
@@ -4,16 +4,25 @@ namespace App\Livewire\Components;
 
 use App\Models\Post;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class Menu extends Component
 {
     public $posts;
+    public $unpublished;
+    public bool $authd;
 
     public function mount()
     {
         $this->posts = Post::where('is_published', true)
             ->orderBy('published_at', 'desc')
             ->get();
+
+        $this->unpublished = Post::where('is_published', false)
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+            $this->authd = Auth::user() ? true : false;
     }
 
     public function render()
--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -3,7 +3,15 @@
 
     <x-link :href="route('authd.posts.create')" text="New Post" i="nf-fa-plus" />
 
+    @if ($authd)
+        <span>-------Unpublished-------</span>
+        @foreach ($unpublished as $post)
+            <x-link :href="route('authd.posts.edit', $post->slug)" text="{{ $post->navDate . ' - ' . $post->title }}" :title="$post->title" />
+        @endforeach
+        <span>-----------------------------</span>
+    @endif
+
     @foreach ($posts as $post)
-        <x-link :href="route('posts.show', $post->slug)" text="{{ $post->navDate . ' - ' . $post->title }}" :title="$post->title"/>
+        <x-link :href="route('posts.show', $post->slug)" text="{{ $post->navDate . ' - ' . $post->title }}" :title="$post->title" />
     @endforeach
 </nav>
