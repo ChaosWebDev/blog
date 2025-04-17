1c891fd1c28aa887c0851ba034b8a4faaa6dc816
jpgerber1986 <jordangerber@gmail.com>
2025-04-17 10:18:39 -0600

    resetting

--- resources/views/components/input.blade.php
+++
@@ -1,6 +0,0 @@
-<label for="{{ $id }}">{{ $label }}</label>
-<input type="{{ $type }}" wire:model="{{ $model }}" name="{{ $name }}" id="{{ $id }}"
-    {{ $attributes }}>
-@error($name)
-    <div class="error">{{ $message }}</div>
-@enderror
